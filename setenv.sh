d106dfac172234ae5e32288e7e565e244db8aa63
Ricardo Alejos <ricardo.alejos@iceye.fi>
2021-10-11 13:58:41 +0300

    Adds a shared variant of the library

--- setenv.sh
+++ setenv.sh
@@ -9,5 +9,5 @@ export LD_LIBRARY_PATH=${PROJECT_OUTPUT}/lib:${LD_LIBRARY_PATH}
 alias mbox_configure="cmake -S ${PROJECT_SOURCE} -B ${PROJECT_BUILD} -DCMAKE_INSTALL_PREFIX=${PROJECT_OUTPUT}"
 alias mbox_dependencies="mbox_configure; make -C ${PROJECT_BUILD} install_dependencies"
 alias mbox_test="make -C ${PROJECT_BUILD} test"
-alias mbox_build="make -C ${PROJECT_BUILD} application; cmake --install ${PROJECT_BUILD}"
+alias mbox_build="make -C ${PROJECT_BUILD}; cmake --install ${PROJECT_BUILD}"
 alias mbox_clean="rm -rf out"
